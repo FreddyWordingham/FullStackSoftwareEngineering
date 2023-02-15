3331adacae45f237a562b35af4459185b0e846cd
FreddyWordingham <freddy@digilab.co.uk>
2023-02-15 21:57:17 +0000

    Added hello route

--- le/2_backend/api/main.py
+++ le/2_backend/api/main.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    return "Hello, world!"
+
+
+@app.get("/{name}")
+async def hello(name: str):
+    return f"Hello, {name}!"
