7840b83f5d4de55b4d05903ed71e6e4ca87207d3
FreddyWordingham <freddy@digilab.co.uk>
2023-02-16 21:30:13 +0000

    Added .env settings

--- le/2_backend/api/settings.py
+++ le/2_backend/api/settings.py
@@ -1,4 +1,16 @@
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
 
 
+class Environment(BaseSettings):
+    SECRET_KEY: str
+
+    class Config:
+        env_prefix = ""
+        case_sensitive = False
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+ENV = Environment()
 TEMPLATES = Jinja2Templates(directory="templates")
